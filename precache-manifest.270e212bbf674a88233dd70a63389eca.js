731e343fced49f35d9d2da2e8f597d51c2a29a8d
Ana de Oliveira Rodrigues <ana.oliveira.rodrigues@gmail.com>
2020-10-09 14:37:04 -0400

    Updates

---
+++ precache-manifest.270e212bbf674a88233dd70a63389eca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb30c43230720e9a6ef788a2b839d1f3",
+    "url": "/anarodrigues/testFirebaseDB.git/index.html"
+  },
+  {
+    "revision": "cf2bce3a04472a07339f",
+    "url": "/anarodrigues/testFirebaseDB.git/static/css/main.752c43f0.chunk.css"
+  },
+  {
+    "revision": "5e9ef5e7e28641726b7d",
+    "url": "/anarodrigues/testFirebaseDB.git/static/js/2.d3f894ef.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/anarodrigues/testFirebaseDB.git/static/js/2.d3f894ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf2bce3a04472a07339f",
+    "url": "/anarodrigues/testFirebaseDB.git/static/js/main.9a4cf353.chunk.js"
+  },
+  {
+    "revision": "b8abe2b02c201bdcdb39",
+    "url": "/anarodrigues/testFirebaseDB.git/static/js/runtime-main.834fd8d4.js"
+  }
+]);
